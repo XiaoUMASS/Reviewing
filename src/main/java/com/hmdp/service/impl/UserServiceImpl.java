41477599dfd17395a8f056f603b65284dbd5c2da
XiaoUMASS <xwang6@umass.edu>
2024-09-21 01:23:34 +0800

    重置初始代码*2

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -6,14 +6,7 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- * 服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
